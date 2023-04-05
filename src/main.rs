66fccae4eec2461ec317c6b825c7280b0bbd0578
SanderLoman <sanderfeitsma13@gmail.com>
2023-04-05 14:57:39 +0200

    updated amount of threads

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use rand::Rng;
+use rayon::prelude::*;
 use secp256k1::PublicKey;
 use sha3::{Digest, Keccak256};
+use std::sync::atomic::{AtomicU32, Ordering};
+use std::time::Instant;
 
-const STEP: u32 = 1;
+const STEP: u32 = 10000;
 
 fn private_to_address(private_key: &[u8]) -> String {
     let secp = secp256k1::Secp256k1::new();
@@ -23,7 +26,7 @@ fn get_random_wallet() -> (String, String) {
     (address, hex::encode(private_key))
 }
 
-fn is_valid_vanity_address(address: &str, input: &str, is_checksum: bool, is_suffix: bool) -> bool {
+fn is_valid_address(address: &str, input: &str, is_checksum: bool, is_suffix: bool) -> bool {
     let sub_str = if is_suffix {
         &address[40 - input.len()..]
     } else {
@@ -83,49 +86,52 @@ fn to_checksum_address(address: &str) -> String {
     ret
 }
 
-fn get_vanity_wallet(
+fn workers(
     input: &str,
     is_checksum: bool,
     is_suffix: bool,
-) -> Result<(String, String, u32), String> {
-    let input = if is_checksum {
-        input.to_string()
-    } else {
-        input.to_lowercase()
-    };
-    let mut attempts: u32 = 0;
+    num_threads: usize,
+) -> (String, String, u32) {
+    let attempts = AtomicU32::new(0);
 
-    loop {
-        let (address, private_key) = get_random_wallet();
-        attempts += 1;
+    let result = (0..num_threads)
+        .into_par_iter()
+        .map(|_| loop {
+            let (address, private_key) = get_random_wallet();
+            attempts.fetch_add(1, Ordering::Relaxed);
 
-        if is_valid_vanity_address(&address, &input, is_checksum, is_suffix) {
-            return Ok((
-                "0x".to_string() + &to_checksum_address(&address),
-                private_key,
-                attempts,
-            ));
-        }
+            if is_valid_address(&address, input, is_checksum, is_suffix) {
+                break (address, private_key);
+            }
 
-        if attempts % STEP == 0 {
-            println!("Attempts: {}", attempts);
-        }
-    }
+            if attempts.load(Ordering::Relaxed) % STEP == 0 {
+                println!("Attempts: {}", attempts.load(Ordering::Relaxed));
+            }
+        })
+        .find_any(|_| true)
+        .unwrap();
+
+    (
+        "0x".to_string() + &to_checksum_address(&result.0),
+        result.1,
+        attempts.load(Ordering::Relaxed),
+    )
 }
+
 fn main() {
-    let input = "0000"; // The desired prefix or suffix choose from  e.g. "A B C D E F" and "0 1 2 3 4 5 6 7 8 9"
+    let input = "000"; // The desired prefix or suffix choose from  e.g. "A B C D E F" and "0 1 2 3 4 5 6 7 8 9"
     let is_checksum = false; // Whether the input is case-sensitive
     let is_suffix = false; // Whether the input is a suffix or a prefix. true for suffix (meaning at the end of the address)
+    let num_threads = 16; // The number of threads you want to use
 
-    match get_vanity_wallet(input, is_checksum, is_suffix) {
-        Ok((address, private_key, attempts)) => {
-            println!(
-                "Address: {}\nPrivate Key: {}\nAttempts: {}",
-                address, private_key, attempts
-            );
-        }
-        Err(err) => {
-            eprintln!("Error: {}", err);
-        }
-    }
+    let start_time = Instant::now();
+    let (address, private_key, attempts) = workers(input, is_checksum, is_suffix, num_threads);
+    let elapsed_time = start_time.elapsed();
+
+    println!(
+        "\nAddress: {}\nPrivate Key: {}\n\nAttempts: {}\n",
+        address, private_key, attempts
+    );
+
+    println!("Time taken: {} seconds", elapsed_time.as_secs_f64());
 }
